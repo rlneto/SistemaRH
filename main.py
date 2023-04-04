650409d5af0dbf8e94613f7b54d4157c42ec4be9
Rudolfo <rlneto@live.com>
2023-04-03 21:45:49 -0300

    Commit abstract

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from cargo import Cargo
 from funcionario import Funcionario
 
+
+def tracejado():
+    print("-" * 79)
+def relatorio_pessoas(pessoas):
+    print("Pessoas da empresa:\n")
+    for pessoa in pessoas:
+        print(pessoa.nome, pessoa.cpf)
+    tracejado()
+
+def relatorio_dependentes(funcionario):
+    print("\nDependentes de", funcionario.nome)
+    for dep in funcionario.dependentes:
+        print(dep.nome)
+    tracejado()
+
 if __name__ == '__main__':
     bancario = Cargo(4000, 'bancário')
     joao = Funcionario("João", "1234", bancario)
